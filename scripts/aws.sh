c0bbe24c169df336a6e2d9383d4d537de279092a
sirishkumar <sirishkumar@gmail.com>
2017-10-15 21:59:39 +0000

    added my aws sccript

---
+++ scripts/aws.sh
@@ -0,0 +1,13 @@
+!#/bin/sh
+
+sudo mkdir -p /fastai
+sudo mount /dev/xvdh /fastai
+ssh-agent /bin/bash
+ssh-add /fastai/.ssh/id_rsa
+
+# Install dot files
+cd /fastai/tools/dotfiles
+make
+
+
+cd /fastai/
